ce107b871927df568148068817356e72e43ab939
sdram58 <sdram58@hotmail.com>
2017-06-05 02:40:04 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { MainPage } from '../pages/main/main';
 import { AboutPage } from '../pages/about/about';
 import { DetailPage } from '../pages/detail/detail';
+import { OrderDetailPage } from '../pages/order-detail/order-detail';
 import { ProductListPage } from '../pages/product-list/product-list';
-import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { AuthService } from '../providers/auth-service/auth-service';
 import { ProductServiceProvider } from '../providers/product-service/product-service';
+import { OrderServiceProvider } from '../providers/order-service/order-service';
 
 @NgModule({
   declarations: [
@@ -20,10 +24,13 @@ import { ProductServiceProvider } from '../providers/product-service/product-ser
     MainPage,
     DetailPage,
     ProductListPage,
-    AboutPage,      
+    AboutPage,
+    OrderDetailPage,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -33,14 +40,17 @@ import { ProductServiceProvider } from '../providers/product-service/product-ser
     MainPage,
     DetailPage,
     ProductListPage,
-    AboutPage,  
+    AboutPage,
+    OrderDetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthServiceProvider,
-    ProductServiceProvider
+    AuthService,
+    ProductServiceProvider,
+    OrderServiceProvider
   ]
 })
 export class AppModule {}
+
